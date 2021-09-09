22267a203c74844e2c39801c9b4e9ab31f86a269
Stephan <stephan.steidl903@gmail.com>
2021-09-09 20:40:53 +0200

    kein plan aber es geht mal ned

--- main.py
+++ main.py
@@ -1,70 +1,79 @@
-# k-nearest neighbors on the Iris Flowers Dataset
-from random import randrange
-from csv import reader
-from math import sqrt
+from collections import Counter
+
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-from numpy.random import randint
 
 
-def euclidean_distance(x_test, x_train):
-    distance = 0.0
-    for i in range(len(x_test) - 1):
-        distance += (x_test[i] - x_train[i]) ** 2
-    return sqrt(distance)
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum((x1 - x2) ** 2))
 
 
-def knn(row, x_train, y_train, k):
-    label = []
-    neigbour_dist = []
-    # Calc dist for all data
-    for i in range(0, len(x_train)):
-        dist = euclidean_distance(row, x_train[i])
-        neigbour_dist.append(dist)
+class KNN:
+    def __init__(self, k=3):
+        self.k = k
 
-    # find the k nearest points
-    ndist = np.array(neigbour_dist)
-    knn = ndist.argsort()[:k]
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
 
-    # find labels for nearest points
-    for i in knn:
-        label.append(y_train[i])
-    # return most common label
-    return max(set(label), key=label.count)
+    def predict(self, X):
+        #find the likely labels for the data in X and return it in an array
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
 
-
-def predict(x_test, x_train, y_train, k):
-    predictions = []
-    for row in x_test:
-        label = knn(row, x_train, y_train, k)
-        predictions.append(label)
-    return predictions
+    def _predict(self, x):
+        # Compute distances between x and all examples in the training set
+        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+        # Sort by distance and return indices of the k neighbors
+        k_idx = np.argsort(distances)[: self.k]
+        # Extract the labels of the k nearest neighbor training samples
+        k_neighbor_labels = [self.y_train[i] for i in k_idx]
+        # return the most common class label
+        most_common = Counter(k_neighbor_labels).most_common(1)
+        return most_common[0][0]
 
 
-data_headers = ['sepal length', 'sepal width', 'petal length', 'petal width', 'class']
-data = pd.read_csv("iris.data", names=data_headers)
+if __name__ == "__main__":
+    # Imports
+    from matplotlib.colors import ListedColormap
+    #from sklearn import datasets
+    #from sklearn.model_selection import train_test_split
 
-#split data into k arrays and get train and test xy
-folds = 5
-for i in range(folds):
-    #split das zeug ab
-    #in die 4 train und test
-
-    #mach knn mit train und speichers
-
-    #dann mach knn nochmal aber in predict method
-    #predictions = predict(X_train, y_train, X_test, 3)
-    #vergelcihe das predictete mit dem echten??
+    def accuracy(y_true, y_pred):
+        accuracy = np.sum(y_true == y_pred) / len(y_true)
+        return accuracy
+    def getFolds(i, datasize, data):
+        start = i * datasize
+        end = start + datasize
+        train = data.drop(start, end)
+        test = data.iloc([start:end])
 
 
 
+    #iris = datasets.load_iris()
+    #X, y = iris.data, iris.target
+
+    #X_train, X_test, y_train, y_test = train_test_split(
+    #    X, y, test_size=0.2, random_state=1234
+    #)
+
+    data_headers = ['sepal length', 'sepal width', 'petal length', 'petal width', 'class']
+    data = pd.read_csv("data/iris.data", names=data_headers)
+
+    features = ['sepal length', 'sepal width', 'petal length', 'petal width']
+    X = data[features].split()
+    y = data['class'].split()
+
+    #für folds
+    folds = 5
+    datasize = int(X.shape[0] / folds)
+    liste = list()
+    for i in folds:
 
 
 
-#[plt.scatter(X[i][0], X[i][1], color='y') for i in range(len(y))]
-#[plt.scatter(X[i][0], X[i][1], color='b' if y[i] == 0 else 'r') for i in range(len(y))]
-#plt.show()
-
-#split into folds => 150 und man nimmt immer 15 weg zb und hat dadurch 10 verschiedene sets an daten
-# diese daten mit knn berechnen und dann am schluss mit acutal wert vergleichen
+    k = 3
+    clf = KNN(k=k)
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+    print("KNN classification accuracy", accuracy(y_test, predictions))
