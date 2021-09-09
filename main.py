7244d799c2512c4f6b19b62d4436cc797bcb9a22
Stephan <stephan.steidl903@gmail.com>
2021-09-09 16:36:05 +0200

    hä

--- main.py
+++ main.py
@@ -1,153 +1,70 @@
 # k-nearest neighbors on the Iris Flowers Dataset
-from random import seed
 from random import randrange
 from csv import reader
 from math import sqrt
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from numpy.random import randint
 
 
-# Load a CSV file
-def load_csv(filename):
-    dataset = list()
-    with open(filename, 'r') as file:
-        csv_reader = reader(file)
-        for row in csv_reader:
-            if not row:
-                continue
-            dataset.append(row)
-    return dataset
-
-
-# Convert string column to float
-def str_column_to_float(dataset, column):
-    for row in dataset:
-        row[column] = float(row[column].strip())
-
-
-# Convert string column to integer
-def str_column_to_int(dataset, column):
-    class_values = [row[column] for row in dataset]
-    unique = set(class_values)
-    lookup = dict()
-    for i, value in enumerate(unique):
-        lookup[value] = i
-    for row in dataset:
-        row[column] = lookup[row[column]]
-    return lookup
-
-
-# Find the min and max values for each column
-def dataset_minmax(dataset):
-    minmax = list()
-    for i in range(len(dataset[0])):
-        col_values = [row[i] for row in dataset]
-        value_min = min(col_values)
-        value_max = max(col_values)
-        minmax.append([value_min, value_max])
-    return minmax
-
-
-# Rescale dataset columns to the range 0-1
-def normalize_dataset(dataset, minmax):
-    for row in dataset:
-        for i in range(len(row)):
-            row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
-
-
-# Split a dataset into k folds
-def cross_validation_split(dataset, n_folds):
-    dataset_split = list()
-    dataset_copy = list(dataset)
-    fold_size = int(len(dataset) / n_folds)
-    for _ in range(n_folds):
-        fold = list()
-        while len(fold) < fold_size:
-            index = randrange(len(dataset_copy))
-            fold.append(dataset_copy.pop(index))
-        dataset_split.append(fold)
-    return dataset_split
-
-
-# Calculate accuracy percentage
-def accuracy_metric(actual, predicted):
-    correct = 0
-    for i in range(len(actual)):
-        if actual[i] == predicted[i]:
-            correct += 1
-    return correct / float(len(actual)) * 100.0
-
-
-# Evaluate an algorithm using a cross validation split
-def evaluate_algorithm(dataset, algorithm, n_folds, *args):
-    folds = cross_validation_split(dataset, n_folds)
-    scores = list()
-    for fold in folds:
-        train_set = list(folds)
-        train_set.remove(fold)
-        train_set = sum(train_set, [])
-        test_set = list()
-        for row in fold:
-            row_copy = list(row)
-            test_set.append(row_copy)
-            row_copy[-1] = None
-        predicted = algorithm(train_set, test_set, *args)
-        actual = [row[-1] for row in fold]
-        accuracy = accuracy_metric(actual, predicted)
-        scores.append(accuracy)
-    return scores
-
-
-# Calculate the Euclidean distance between two vectors
-def euclidean_distance(row1, row2):
+def euclidean_distance(x_test, x_train):
     distance = 0.0
-    for i in range(len(row1) - 1):
-        distance += (row1[i] - row2[i]) ** 2
+    for i in range(len(x_test) - 1):
+        distance += (x_test[i] - x_train[i]) ** 2
     return sqrt(distance)
 
 
-# Locate the most similar neighbors
-def get_neighbors(train, test_row, num_neighbors):
-    distances = list()
-    for train_row in train:
-        dist = euclidean_distance(test_row, train_row)
-        distances.append((train_row, dist))
-    distances.sort(key=lambda tup: tup[1])
-    neighbors = list()
-    for i in range(num_neighbors):
-        neighbors.append(distances[i][0])
-    return neighbors
+def knn(row, x_train, y_train, k):
+    label = []
+    neigbour_dist = []
+    # Calc dist for all data
+    for i in range(0, len(x_train)):
+        dist = euclidean_distance(row, x_train[i])
+        neigbour_dist.append(dist)
+
+    # find the k nearest points
+    ndist = np.array(neigbour_dist)
+    knn = ndist.argsort()[:k]
+
+    # find labels for nearest points
+    for i in knn:
+        label.append(y_train[i])
+    # return most common label
+    return max(set(label), key=label.count)
 
 
-# Make a prediction with neighbors
-def predict_classification(train, test_row, num_neighbors):
-    neighbors = get_neighbors(train, test_row, num_neighbors)
-    output_values = [row[-1] for row in neighbors]
-    prediction = max(set(output_values), key=output_values.count)
-    return prediction
+def predict(x_test, x_train, y_train, k):
+    predictions = []
+    for row in x_test:
+        label = knn(row, x_train, y_train, k)
+        predictions.append(label)
+    return predictions
 
 
-# kNN Algorithm
-def k_nearest_neighbors(train, test, num_neighbors):
-    predictions = list()
-    for row in test:
-        output = predict_classification(train, row, num_neighbors)
-        predictions.append(output)
-    return (predictions)
+data_headers = ['sepal length', 'sepal width', 'petal length', 'petal width', 'class']
+data = pd.read_csv("iris.data", names=data_headers)
+
+#split data into k arrays and get train and test xy
+folds = 5
+for i in range(folds):
+    #split das zeug ab
+    #in die 4 train und test
+
+    #mach knn mit train und speichers
+
+    #dann mach knn nochmal aber in predict method
+    #predictions = predict(X_train, y_train, X_test, 3)
+    #vergelcihe das predictete mit dem echten??
 
 
-# Test the kNN on the Iris Flowers dataset
-#seed(1)
-filename = 'iris.csv'
-dataset = load_csv(filename)
-
-#for i in range(len(dataset[0]) - 1):
-#    str_column_to_float(dataset, i)
-# convert class column to integers
-#str_column_to_int(dataset, len(dataset[0]) - 1)
-# evaluate algorithm
 
 
-n_folds = 5
-num_neighbors = 5
-scores = evaluate_algorithm(dataset, k_nearest_neighbors, n_folds, num_neighbors)
-print('Scores: %s' % scores)
-print('Mean Accuracy: %.3f%%' % (sum(scores) / float(len(scores))))
+
+
+#[plt.scatter(X[i][0], X[i][1], color='y') for i in range(len(y))]
+#[plt.scatter(X[i][0], X[i][1], color='b' if y[i] == 0 else 'r') for i in range(len(y))]
+#plt.show()
+
+#split into folds => 150 und man nimmt immer 15 weg zb und hat dadurch 10 verschiedene sets an daten
+# diese daten mit knn berechnen und dann am schluss mit acutal wert vergleichen
